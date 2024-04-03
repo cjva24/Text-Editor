ac91006fc0baa30f84ccb5de77636d80ca5694a2
Cjva24 <cjvanauken2424@gmail.com>
2024-04-03 11:47:20 -0500

    files and code added

--- develop/client/src/js/database.js
+++ develop/client/src/js/database.js
@@ -12,10 +12,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  try {
+    console.log('PUT to the db');
+    const jateDb = await openDB('jate', 1);
+    const rw = jateDb.transaction('jate', 'readwrite');
+    const store = rw.objectStore('jate');
+    const req = store.put({ content });
+    const res = await req;
+    console.log('New record', res);
+  } catch(err) {
+    console.error('Failed to get data from the jate database', err);
+  }
+
+};
+
+export const getDb = async () => {
+  try {
+  console.log('GET to the db');
+  const jateDb = await openDB('jate', 1);
+  const ro = jateDb.transaction('jate', 'readonly');
+  const store = ro.objectStore('jate');
+  const req = store.getAll();
+  const res = await req;
+  console.log('Data retrieved from db', res);
+  } catch (err) {
+  console.log('Failed to retrieve data from db');
+  }
+};
 
 initdb();
